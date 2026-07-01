d1a08aec98bb98cb84ea386e6bc68e36d6dda331
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-07-01 18:52:47 +0530

    Time: 0 ms (100%), Space: 12.3 MB (54.32%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isUgly(self, n):
+        if n<=0:
+            return False
+        for f in [2,3,5]:
+            while n%f==0:
+                n//=f
+        return n==1
+        
