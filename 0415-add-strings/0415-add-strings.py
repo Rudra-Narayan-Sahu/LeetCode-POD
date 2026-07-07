ce851d244c7b8ba4e1bea383ad33ea7cf516ad2f
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-07-07 10:04:33 +0530

    Time: 4 ms (54.94%), Space: 19.3 MB (70.26%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        i=len(num1)-1
+        j=len(num2)-1
+        res=[]
+        carry=0
+        while i>=0 or j>=0 or carry:
+            digit1=int(num1[i]) if i>=0 else 0
+            digit2=int(num2[j]) if j>=0 else 0
+            total=digit1+digit2+carry
+            res.append(total%10)
+            carry=total//10
+            i-=1
+            j-=1
+        s=""
+        for el in reversed(res):
+            s+=str(el)
+        return s
