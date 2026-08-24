2f28165ba52d5ccf74d4091293a8b40266ed1ddb
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-08-24 10:12:54 +0530

    Time: 720 ms (20.71%), Space: 33 MB (63.91%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n=len(stones)
+        pre=[0]*n
+        pre[0]=stones[0]
+        for i in range(1,n):
+            pre[i]=pre[i-1]+stones[i]
+        f=[0]*n
+        f[n-1]=pre[n-1]
+        for i in range(n-2,0,-1):
+            f[i]=max(f[i+1],pre[i]-f[i+1])
+        return f[1]
+        
