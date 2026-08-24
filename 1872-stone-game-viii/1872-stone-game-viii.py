d1e749fa7ca540975050b5793fda9bbfefd8360d
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-08-24 10:14:18 +0530

    Time: 695 ms (40.24%), Space: 33.5 MB (24.85%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.py
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -1,10 +1,11 @@
 class Solution:
     def stoneGameVIII(self, stones: List[int]) -> int:
         n=len(stones)
-        pre=[0]*n
-        pre[0]=stones[0]
-        for i in range(1,n):
-            pre[i]=pre[i-1]+stones[i]
+        # pre=[0]*n
+        # pre[0]=stones[0]
+        # for i in range(1,n):
+        #     pre[i]=pre[i-1]+stones[i]
+        pre=list(accumulate(stones))
         f=[0]*n
         f[n-1]=pre[n-1]
         for i in range(n-2,0,-1):
