ebe8f6fa2bbd2bbcd8341cee8f4650c3a438d5f1
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-09-02 08:50:12 +0530

    Time: 0 ms (100%), Space: 19.3 MB (47.47%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # n=len(nums1)
+        # nums2_odd=[None]*n
+        # nums2_even=[None]*n
+        # for i in range(n):
+        #     #case-1
+        #     if nums1[i]%2==0:
+        #         nums2_even[i]=nums1[i]
+        #     else:
+        #         nums2_odd[i]=nums1[i]
+        return True
+            
