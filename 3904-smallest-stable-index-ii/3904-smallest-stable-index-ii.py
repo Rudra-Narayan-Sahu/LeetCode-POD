30b647160f90f141dfdcf3f152a264ffd452a8af
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-09-04 10:48:59 +0530

    Time: 70 ms (100%), Space: 33.2 MB (32.73%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -1,17 +1,16 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-            res=float('inf')
-            n=len(nums)
-            max_res=[None]*n
-            min_res=[None]*n
-            max_res[0]=nums[0]
-            min_res[-1]=nums[-1]
-            for i in range(1,n):
-                max_res[i]=max(max_res[i-1],nums[i])
-            for j in range(n-2,-1,-1):
-                min_res[j]=min(min_res[j+1],nums[j])
-            for i in range(len(nums)):
-                score=max_res[i]-min_res[i]
-                if score<=k and score<res:
-                    return i
-            return -1
+        n=len(nums)
+        mini=[None]*n
+        mn=float('inf')
+        for i in range(n-1,-1,-1):
+            if nums[i]<mn:
+                mn=nums[i]
+            mini[i]=mn
+        mx=float('-inf')
+        for i in range(n):
+            if nums[i]>mx:
+                mx=nums[i]
+            if mx-mini[i]<=k:
+                return i
+        return -1
