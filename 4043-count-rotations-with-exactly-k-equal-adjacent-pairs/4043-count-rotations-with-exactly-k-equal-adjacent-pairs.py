584248ac8a78a03ec2ae018b49d0f06fa726807b
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-09-06 15:53:37 +0530

    Time: 3 ms (92.24%), Space: 12.3 MB (61.87%) - LeetHub

--- 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.py
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.py
@@ -2,14 +2,18 @@ class Solution(object):
     def countRotations(self, s, k):
         total=0
         n=len(s)
-        for i in range(len(s)):
-            t=s[i:]+s[:i]
-            score=0
-            for j in range(len(t)-1):
-                if t[j]==t[j+1]:
-                    score+=1
-            if score==k:
+        for i in range(n-1):
+            if s[i]==s[i+1]:
                 total+=1
-        return total
+        if s[-1]==s[0]:
+            total+=1
+        if k==total:
+            return n-total
+        elif k==total-1:
+            return total
+        else:
+            return 0
 
+        
+        
         
