86af23d68503c8d0c53300faa7eb7cfb02ed467f
Rudra Narayan Sahu <rudrasahu9007@gmail.com>
2026-09-11 10:03:57 +0530

    Time: 21 ms (79.36%), Space: 12.4 MB (26.19%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -6,13 +6,13 @@ class Solution(object):
         """
         res=set()
         n=len(digits)
-        for i in range(n):
+        for i in range(n):#100th placee
             if digits[i]==0:
                 continue
-            for j in range(n):
+            for j in range(n):#10th place
                 if i==j:
                     continue
-                for k in range(n):
+                for k in range(n):#once place
                     if k==i or k==j:
                         continue
                     if digits[k]%2==0:
